3688c399c6851499d4ffb1de88a0a1c5089cece4
Sinebi Innazo <sinebi.innazo@gmail.com>
2022-08-13 09:58:02 +0100

    added Book service

---
+++ src/main/java/com/lms/lms/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.lms.lms.repository;
+
+import com.lms.lms.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book,Long> {
+}
