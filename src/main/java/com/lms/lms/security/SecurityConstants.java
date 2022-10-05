b59b645af33c8c501fddfb9a2d66d86c1659d8b6
unknown <sinebi.innazo@gmail.com>
2022-10-05 01:04:38 -0700

    Modified the Users endpoint

--- src/main/java/com/lms/lms/security/SecurityConstants.java
+++ src/main/java/com/lms/lms/security/SecurityConstants.java
@@ -6,7 +6,7 @@ public class SecurityConstants {
     public static final long EXPIRATION_TIME = 864000000;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final String SIGNUP_URL = "/api/v1/users/**";
+    public static final String SIGNUP_URL = "/users/**";
     public static final String BOOK_URL = "/api/v1/book/**";
     public static final String AUTHOR_URL = "/api/v1/author/**";
     public static final String PUBLISHER_URL = "/api/v1/publisher/**";
